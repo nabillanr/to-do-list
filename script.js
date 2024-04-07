9b9fbdcf451b58ca91cbe174cdb150483758e562
NaurahKamilaSyadza <naurahkamila26@gmail.com>
2024-04-07 23:44:06 +0700

    update

--- script.js
+++ script.js
@@ -69,3 +69,57 @@ function UpdateTextOnClick(){
         listItemToUpdate.removeAttribute("contenteditable");
     }
 }
+
+// Fungsi untuk mengambil daftar "to-do list" dari server saat halaman dimuat
+window.onload = function() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(todo => {
+                createTodoItem(todo.title, todo.completed);
+            });
+        })
+        .catch(error => {
+            console.error('Gagal mengambil data dari server:', error);
+        });
+};
+
+// Fungsi untuk membuat item "to-do list" baru
+function createTodoItem(text, completed) {
+    let li = document.createElement("li");
+    const todoItems = `<div onclick="CompleteTodoItem(this)" ${completed ? 'style="text-decoration: line-through;"' : ''}>${text}</div><div><img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="/images/pencil.png" /><img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="/images/delete.png"/></div>`;
+
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+}
+
+// Fungsi untuk menyimpan "to-do list" ke server
+function saveTodoList() {
+    const todos = [];
+    document.querySelectorAll('#list-items > li > div:first-child').forEach(todo => {
+        todos.push({
+            title: todo.innerText,
+            completed: todo.style.textDecoration === "line-through"
+        });
+    });
+
+    // Kirim data "to-do list" ke server menggunakan metode POST
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body: JSON.stringify(todos),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Data berhasil disimpan:', data);
+    })
+    .catch(error => {
+        console.error('Gagal menyimpan data:', error);
+    });
+}
+
+// Panggil fungsi saveTodoList setiap kali ada perubahan pada "to-do list"
+listItems.addEventListener('DOMSubtreeModified', saveTodoList);
+
